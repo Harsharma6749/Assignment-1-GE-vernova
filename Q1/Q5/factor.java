b8b866b53358ed086e1fe59fd9ca77bacd95d012
Harsharma6749 <harshsharma6749@gmail.com>
2025-12-19 12:41:17 +0530

    Create factor.java

---
+++ Q1/Q5/factor.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class factor{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.print("Prime factors of " + n + " are: ");
+
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                System.out.print(i + " ");
+                n /= i;
+            }
+        }
+
+        if (n > 1) {
+            System.out.print(n);
+        }
+    }
+}
